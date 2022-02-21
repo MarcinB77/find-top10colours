c47e3e439515b15408f8d9c0b3fcb2892415f197
MarcinB77 <baranskimarcin.mb@gmail.com>
2022-02-21 20:30:44 +0100

    full project

---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+import glob
+import numpy as np
+from PIL import Image
+import pandas as pd
+from sklearn.cluster import KMeans
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from werkzeug.utils import secure_filename, redirect
+from wtforms import SubmitField
+
+
+class PhotoForm(FlaskForm):
+    photo = FileField(validators=[FileRequired()])
+    submit = SubmitField("Upload")
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "verysecretkey"
+
+Bootstrap(app)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    files = glob.glob('static/uploads/*')
+    for f in files:
+        os.remove(f)
+    form = PhotoForm()
+    if form.validate_on_submit():
+        file = form.photo.data
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(f"static/uploads/{filename}")
+            return redirect(url_for('show', file=filename))
+
+    return render_template('index.html', form=form)
+
+
+@app.route('/<file>')
+def show(file):
+    colors = sample_colors(f"static/uploads/{file}")
+    return render_template('index.html', file=file, colors=colors)
+
+
+def sample_colors(file_path):
+    uploaded_img = Image.open(file_path)
+    img_array = np.array(uploaded_img)
+    try:
+        colors_array = np.reshape(img_array, (img_array.shape[0] * img_array.shape[1], 3))
+    except ValueError:
+        rgb_im = uploaded_img.convert('RGB')
+        rgb_img_array = np.array(rgb_im)
+        colors_array = np.reshape(rgb_img_array, (img_array.shape[0] * img_array.shape[1], 3))
+
+    clt = KMeans(n_clusters=10)
+    clt.fit(colors_array)
+
+    colors_df = pd.DataFrame(clt.cluster_centers_)
+
+    top10colors = colors_df.groupby(colors_df.columns.tolist(), as_index=False).size() \
+        .sort_values(by='size', ascending=False).reset_index(drop=True)
+    top10colors = top10colors.astype({0: "int", 1: "int", 2: "int"})
+
+    rgb_list = [(row[0], row[1], row[2]) for index, row in top10colors.iterrows()]
+
+    return rgb_list
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
